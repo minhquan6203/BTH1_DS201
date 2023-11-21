1a59c430c2277ab2a7be1980a868e0b316140809
minhquan6203 <harryquan.minh@gmail.com>
2023-11-21 09:22:23 +0700

    update

--- TH3/model/CNN.py
+++ TH3/model/CNN.py
@@ -117,17 +117,75 @@ class ResNet50_Model(nn.Module):
         x = F.softmax(x, dim=-1)
         return x
 
+# class ResNet18_Model(nn.Module):
+#     def __init__(self, config):
+#         super(ResNet18_Model, self).__init__()
+#         self.num_classes = config['num_classes']
+#         self.cnn = models.resnet18(pretrained=config['load_pretrained'])
+#         self.cnn.fc = nn.Linear(self.cnn.fc.in_features, self.num_classes)
+        
+#     def forward(self, x):
+#         x = self.cnn(x)
+#         x = F.softmax(x, dim=-1)
+#         return x
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
 class ResNet18_Model(nn.Module):
     def __init__(self, config):
         super(ResNet18_Model, self).__init__()
-        self.num_classes = config['num_classes']
-        self.cnn = models.resnet18(pretrained=config['load_pretrained'])
-        self.cnn.fc = nn.Linear(self.cnn.fc.in_features, self.num_classes)
-        
+        self.in_planes = 64
+
+        # Initial Convolution layers
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(BasicBlock, 64, 2, stride=1)
+        self.layer2 = self._make_layer(BasicBlock, 128, 2, stride=2)
+        self.layer3 = self._make_layer(BasicBlock, 256, 2, stride=2)
+        self.layer4 = self._make_layer(BasicBlock, 512, 2, stride=2)
+        self.linear = nn.Linear(512*BasicBlock.expansion, config['num_classes'])
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
     def forward(self, x):
-        x = self.cnn(x)
-        x = F.softmax(x, dim=-1)
-        return x
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        out = F.softmax(out,dim=-1)
+        return out
 
 class LeNet5(nn.Module):
     def __init__(self,config):
@@ -135,30 +193,24 @@ class LeNet5(nn.Module):
         self.image_C = config['image_C']
         self.num_classes = config['num_classes']
 
-        #các lớp convolution
         self.conv1 = nn.Conv2d(self.image_C, 6, 5, padding=2)
         self.conv2 = nn.Conv2d(6, 16, 5)
 
-        #các lớp linear
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, self.num_classes)
 
     def forward(self, x):
-        # lớp convolution thứ nhất
         x = self.conv1(x)
         x = nn.functional.relu(x)
         x = nn.functional.max_pool2d(x, 2)
 
-        # lớp convolution thứ hai
         x = self.conv2(x)
         x = nn.functional.relu(x)
         x = nn.functional.max_pool2d(x, 2)
-
-        # flatten tensor trước khi đưa vào lớp Linear
+    
         x = x.view(-1, 16 * 5 * 5)
 
-        #lớp fully connected
         x = self.fc1(x)
         x = nn.functional.relu(x)
         x = self.fc2(x)
